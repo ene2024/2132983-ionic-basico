c766e783b97c14d8257f6a22df0a0ff93eab5fc3
DanielRojas1920 <danielrojvilla@gmail.com>
2024-03-19 22:21:06 -0600

    Practica07: Componentes

--- miApp-2132983/src/app/home/home.module.ts
+++ miApp-2132983/src/app/home/home.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
-import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { HomePage } from './home.page';
+import { ListaAlumnosComponent } from '../lista-alumnos/lista-alumnos.component';
 
 
 @NgModule({
@@ -12,8 +13,9 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
   ],
-  declarations: [HomePage]
+
+  declarations: [HomePage, ListaAlumnosComponent],
 })
 export class HomePageModule {}
